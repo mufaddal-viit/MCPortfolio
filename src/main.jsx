2674be33d61281ddd1eb663d4bd5da910d3b09e4
mufaddalpam <mufaddal@pamcorpservices.com>
2026-04-22 15:22:37 +0400

    ..

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./theme/ThemeProvider.jsx";
-
+import { Analytics } from "@vercel/analytics/next";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Analytics />
     <BrowserRouter>
       <ThemeProvider>
         <App />
       </ThemeProvider>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
